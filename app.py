76afc35b19be6bab7d5fe048f370f6ee8051abd0
ry <yeryanyin@gmail.com>
2026-04-29 08:57:13 -0400

    denis

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import json
 
-def extract(athlete):
-    file = requests.get(f"https://www.strava.com/api/v3/{athlete.lower()}")
+def extract(sort,item):
+    file = requests.get(f"https://www.strava.com/api/v3/{sort.lower()}/{item.lower()}")
     data = file.json()
-    return
+    return {
+        "name": data["name"],
+        "height": data["height"],
+        "weight": data["weight"],
+        "types": [t["type"]["name"] for t in data["types"]]
+    }
