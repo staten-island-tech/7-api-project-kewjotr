8edb896ce282198c228ea5c66dc04f65bf543be9
ry <yeryanyin@gmail.com>
2026-04-29 09:14:12 -0400

    holkstra

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 import json
+import requests
 
 def extract(sort,item):
-    file = requests.get(f"https://www.strava.com/api/v3/{sort.lower()}/{item.lower()}")
+    file = requests.get(f"https://www.pokeapi.co/api/v2/{sort.lower()}/{item.lower()}")
     data = file.json()
     return {
         "name": data["name"],
+        "id": data["id"],
         "height": data["height"],
         "weight": data["weight"],
         "types": [t["type"]["name"] for t in data["types"]]
     }
+take = input()
+out = extract("pokemon",take)
+print(out)
