fdd2b83312d4d661f5f0423c812bcad1f71d7d08
kike <yeryanyin@gmail.com>
2026-04-30 16:13:33 -0400

    Update 1.1

--- app.py
+++ app.py
@@ -4,5 +4,7 @@ import tkinter as tk
 def extract(category,specific):
     if specific == True:
         response = requests.get(f"https://api.weather.gov/{category}/{specific}")
+    else:
+        response = requests.get(f"https://api.weather.gov/{category}")
         
     data = response.json
