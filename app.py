4269d84213dc7832782e4f03a77503008ff2193a
ry <yeryanyin@gmail.com>
2026-04-30 09:11:16 -0400

    restart

--- app.py
+++ app.py
@@ -1,15 +1,3 @@
 import requests
 
 def extract(sort,item):
-    file = requests.get(f"https://www.pokeapi.co/api/v2/{sort.lower()}/{item.lower()}")
-    data = file.json()
-    return {
-        "name": data["name"],
-        "id": data["id"],
-        "height": data["height"],
-        "weight": data["weight"],
-        "types": [t["type"]["name"] for t in data["types"]]
-    }
-take = input()
-out = extract("pokemon",take)
-print(out)
