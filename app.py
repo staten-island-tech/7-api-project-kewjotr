d395be4e8f05e15a150a229de598608750654556
ry <yeryanyin@gmail.com>
2026-05-01 08:49:59 -0400

    Update 1.1.1

--- app.py
+++ app.py
@@ -2,9 +2,18 @@ import requests
 import tkinter as tk
 
 def extract(category,specific):
-    if specific == True:
+    if specific:
         response = requests.get(f"https://api.weather.gov/{category}/{specific}")
     else:
         response = requests.get(f"https://api.weather.gov/{category}")
         
     data = response.json
+    if specific:
+        if category == "offices":
+            return {
+                "id": data["id"],
+                "name": data["name"],
+                "telephone": data["telephone"],
+                "postal": data["postalCode"],
+                "address": data["streetAddress"]
+            }
