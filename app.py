c1a0d1ccbcfc7ab347071bbe01145f645b579146
kike <yeryanyin@gmail.com>
2026-05-01 21:05:51 -0400

    Update 1.1.3

--- app.py
+++ app.py
@@ -18,9 +18,17 @@ def extract(category,specific):
                 "address": data["streetAddress"]
             }
         elif (category == "alerts") and (specific == "active"):
-            return
+            for alertfinder in data["features"]:
+                return {
+                    "event": data["features"]["properties"]["event"],
+                    "office": data["features"]["properties"]["senderName"],
+                    "category": data["features"]["properties"]["severity"],
+                    "certainty": data["features"]["properties"]["certainty"],
+                    "urgency": data["features"]["properties"]["urgency"]
+                }
 
-print("Available Categories""\n""Alerts, Aviation, Glossary, Stations, Offices, Radar, Points, Products, Zones")
+avail = ["Alerts", "Aviation", "Glossary", "Stations", "Offices", "Radar", "Points", "Products", "Zones"]
+print("Available Categories""\n", avail)
 category1 = ("View category""\n")
 while (category1 != "end") or (category1 != "exit"):
     sort = input("Choose item to view")
