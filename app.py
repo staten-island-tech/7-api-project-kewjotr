3ef74923a8ca550c61085a5a57dc2c281a276b65
kike <yeryanyin@gmail.com>
2026-05-03 10:43:09 -0400

    Update 1.4
    Finished singular category indexes.
    Fixed looping issues.
    More bytes.
    Fixed API dictionary indexes.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import requests
-import tkinter as tk
+
 
 def extract(category,specific):
     if specific:
@@ -14,27 +14,72 @@ def extract(category,specific):
                 "id": data["id"],
                 "name": data["name"],
                 "telephone": data["telephone"],
-                "postal": data["postalCode"],
-                "address": data["streetAddress"]
+                "postal": data["address"]["postalCode"],
+                "address": data["address"]["streetAddress"]
             }
         elif category == "alerts":
             if specific == "active":
-                for alertfinder in data["features"]:
+                for activealerts in data["features"]:
                     return {
-                        "event": data["features"]["properties"]["event"],
-                        "office": data["features"]["properties"]["senderName"],
-                        "category": data["features"]["properties"]["severity"],
-                        "certainty": data["features"]["properties"]["certainty"],
-                        "urgency": data["features"]["properties"]["urgency"]
+                        "event": data["features"][activealerts]["properties"]["event"],
+                        "office": data["features"][activealerts]["properties"]["senderName"],
+                        "category": data["features"][activealerts]["properties"]["severity"],
+                        "certainty": data["features"][activealerts]["properties"]["certainty"],
+                        "urgency": data["features"][activealerts]["properties"]["urgency"]
                     }
             elif specific == "types":
                 return data["eventTypes"]
             else:
                 return "Invalid statement."
-        
+        elif (category == "aviation") and (specific == "sigmets"):
+            for aircraft in data["features"]:
+                return {
+                    "aircrafts": data["features"][aircraftindex]["properties"]["sequence"]
+                }
+    else:
+        if category == "glossary":
+            for glossaryindex in data["glossary"]:
+                return {
+                    "term": data["glossary"][glossaryindex]["term"],
+                    "definition": data["glossary"][glossaryindex]["definition"]
+                }
+        elif category == "alerts":
+            for expiredalerts in data["features"]:
+                return {
+                    "event": data["features"][expiredalerts]["properties"]["event"],
+                    "office": data["features"][expiredalerts]["properties"]["senderName"],
+                    "category": data["features"][expiredalerts]["properties"]["severity"],
+                    "certainty": data["features"][expiredalerts]["properties"]["certainty"],
+                    "urgency": data["features"][expiredalerts]["properties"]["urgency"]
+                }
+        elif category == "stations":
+            for stationsindex in data["features"]:
+                return {
+                    "station": data["features"][stationsindex]["properties"]["stationIdentifier"],
+                    "name": data["features"][stationsindex]["properties"]["name"],
+                    "provider": data["features"][stationsindex]["properties"]["provider"]
+                }
+        elif category == "products":
+            for productsindex in data["@graph"]:
+                return {
+                    "ID": data["@graph"][productsindex]["wmoCollectiveId"],
+                    "Office": data["@graph"][productsindex]["issuingOffice"],
+                    "Code": data["@graph"][productsindex]["productCode"]
+                }
+        elif category == "zones":
+            for zoneindex in data["features"]:
+                return {
+                    "id": data["features"][zoneindex]["properties"]["id"],
+                    "county": data["features"][zoneindex]["properties"]["name"],
+                    "state": data["features"][zoneindex]["properties"]["state"]
+                }
+        else:
+            print("Invalid statement.")
 
-list = ["Alerts", "Aviation", "Glossary", "Stations", "Offices", "Radar", "Points", "Products", "Zones"]
-print("Available Categories""\n", avail)
+
+list = ["Alerts", "Aviation", "Glossary", "Stations", "Offices", "Radar", "Products", "Zones"]
+print("Available Categories:""\n", list)
+
 category1 = ("View category""\n")
 while (category1 != "end") or (category1 != "exit"):
     sort = input("Choose item to view")
